7e4ae0b04b7b30a1b31eee736111b9fbf60e0083
MOADproglep <62516414+MOADproglep@users.noreply.github.com>
2023-02-05 17:35:37 +0500

    First commit

---
+++ Spin/Assets/RotationPlatform.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RotationPlatform : MonoBehaviour
+{
+    private float rotationVector;
+    public float speedRotation;
+
+    private void FixedUpdate()
+    {
+        rotationVector = Input.GetAxis("Horizontal");
+        float delta = rotationVector * speedRotation * Time.deltaTime;
+        transform.Rotate(0, 0, delta);
+    }
+}
